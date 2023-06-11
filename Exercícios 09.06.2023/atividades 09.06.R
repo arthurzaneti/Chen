470275408ad53f55ab12f900c75f3b92cfe6b9eb
Arthur <afzaneti1@gmail.com>
2023-06-11 17:47:15 -0300

    Gráfico com diferentes valores de tau. Também brinquei um pouco com as cores

--- Exercícios 09.06.2023/atividades 09.06.R
+++ Exercícios 09.06.2023/atividades 09.06.R
@@ -1,3 +1,5 @@
+library("RColorBrewer")
+
 rchen <- function(lambda, delta, nelementos){
   valores_aleatorios_tau <- runif(nelementos)
   valores_chen <- log((1-(log(1-valores_aleatorios_tau))/delta))^(1/lambda)
@@ -20,9 +22,14 @@ chen_reparametrizada <- function(y, lambda, mu, tau){
 
 lambda <- 0.5
 curve(chen_reparametrizada(x, 0.5, 2, tau) ,from=0, to=20, xlab = "y")
-curve(chen_reparametrizada(x, 0.7, 7, tau) ,from=0, to=20, xlab = "y", add = TRUE, lty = 2)
+curve(chen_reparametrizada(x, 0.7, 7, tau) ,from=0, to=20, xlab = "y", add = TRUE)
 curve(chen_reparametrizada(x, 0.9, 3, tau) ,from=0, to=20, xlab = "y", add = TRUE, lty = 4)
 curve(chen_reparametrizada(x, 1.1, 8, tau) ,from=0, to=20, xlab = "y", add = TRUE, lty = 3)
+
+cores <- brewer.pal(3, "Set1")
+curve(chen_reparametrizada(x, 0.7, 7, 0.7), from=0, to=20, xlab="Densidade de propabilidade", col = cores[1])
+curve(chen_reparametrizada(x, 0.7, 7, 0.5), from=0, to=20, xlab="Densidade de propabilidade", add =TRUE, col = cores[2])
+curve(chen_reparametrizada(x, 0.7, 7, 0.2), from=0, to=20, xlab="Densidade de propabilidade", add =TRUE, col = cores[3])
 
 #note o eixo Y, ele varia de exemplo pra exemplo
 random_chen1 <- rchen(0.5, calcula_omega(0.5, 2, 0.5), 1000000)
