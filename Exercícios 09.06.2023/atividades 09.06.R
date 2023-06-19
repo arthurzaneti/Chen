cfe192734e66e4fb33ed444f3ae5d1190aaae49d
arthur.zaneti <afzaneti1@gmail.com>
2023-06-19 15:58:12 -0300

    Toda simulação de Monte Carlo concluída e Rdata salva

--- Exercícios 09.06.2023/atividades 09.06.R
+++ Exercícios 09.06.2023/atividades 09.06.R
@@ -31,7 +31,7 @@ mus <- c(2,7,3,8)
 
 
 curve(chen_reparametrizada(x, lambdas[1], mus[1], tau) ,from=0, to=20, ylim=c(0,0.5), col = col1[1], ann=FALSE)
-title(main="Função densidade probabilidade reparametrizada", xlab = "y", ylab="Densidade de probabilidade" )
+title(main="Função densidade \n probabilidade reparametrizada", xlab = "y", ylab="Densidade de probabilidade" )
 curve(chen_reparametrizada(x, lambdas[2], mus[2], tau), add = TRUE, col = col1[2])
 curve(chen_reparametrizada(x, lambdas[3], mus[3], tau), add = TRUE, col = col1[3])
 curve(chen_reparametrizada(x, lambdas[4], mus[4], tau), add = TRUE, col = col1[4])
@@ -62,11 +62,11 @@ hist(random_chen4)
 negativo_ll_chen <- function(y, parametros){
   n<- length(y)
   lambda <- parametros[1]
-  delta <- parametros[2]
-  log_verossimilhanca<- n*log(delta) + sum((lambda-1)*log(y)) + delta*(sum(1-exp(y^lambda)))+ sum(y^lambda)
+  mu <- parametros[2]
+  delta<-calcula_delta(lambda, mu, 0.5)
+  log_verossimilhanca<- n*log(delta)+n*log(lambda) + sum((lambda-1)*log(y)) + delta*(sum(1-exp(y^lambda)))+ sum(y^lambda)
   return (-log_verossimilhanca)
 }
-estimacao <- optim(par=c(2,2), fn=negativo_ll_chen, y=random_chen1, method ="BFGS", hessian =TRUE)
-estimacao
-
-
+random_chen1 <- rchen(0.5, calcula_delta(0.5, 2, 0.5), 1000)
+suppressWarnings(estimacao <- optim(par=c(2,2), fn=negativo_ll_chen, y=random_chen1, method ="BFGS", hessian =TRUE))
+estimacao$par
